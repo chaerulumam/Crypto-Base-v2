9fafedfa9a744e88f1a9dc12981c75b15a8866dc
Chaerul Umam <chaerulumamb244@gmail.com>
2023-02-21 23:13:47 +0700

    create Icon Logo

---
+++ src/components/atoms/logoIcon/index.js
@@ -0,0 +1,17 @@
+import logoIcon from "../../../assets/logoIC.svg";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Logo = () => {
+  return (
+    <Link
+      to="/"
+      className="absolute top-[1.5rem] left-[1.5rem] [text:decoration-none] text-lg text-amber-400 hover:text-amber-500 flex items-center"
+    >
+      <img src={logoIcon} alt="Logo" />
+      <span>CryptoBase</span>
+    </Link>
+  );
+};
+
+export default Logo;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Logo from "../../components/atoms/logoIcon";
 
 const Home = () => {
   return (
-    <div>
-      <p>Ini Home</p>
+    <main className="w-full h-full flex flex-col first-letter:content-center items-center relative text-white font-semibold font-nunito">
+      <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
+      <Logo />
       <Outlet />
-    </div>
+    </main>
   );
 };
 
