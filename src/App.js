5b1d9a456247aa40f99c202a5142220a2247a9f4
Chaerul Umam <chaerulumamb244@gmail.com>
2023-02-27 22:35:32 +0700

    trending component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Footer, LogoIcon, Navbar } from "./components";
 import { CryptoProvider } from "./context/CryptoContext";
+import { TrendingProvider } from "./context/TrendingContext";
 
 function App() {
   return (
     <CryptoProvider>
-      <main className="w-full h-full flex flex-col first-letter:content-center items-center relative text-white font-nunito">
-        <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
-        <LogoIcon />
-        <Navbar />
-        <Outlet />
-        <Footer />
-      </main>
+      <TrendingProvider>
+        <main className="w-full h-full flex flex-col first-letter:content-center items-center relative text-white font-nunito">
+          <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
+          <LogoIcon />
+          <Navbar />
+          <Outlet />
+          <Footer />
+        </main>
+      </TrendingProvider>
     </CryptoProvider>
   );
 }
--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CryptoDetails from "./CryptoDetails";
+import TrendingCoinDetails from "./TrendingCoinDetails";
 
-export { Navbar, Footer, CryptoDetails };
+export { Navbar, Footer, CryptoDetails, TrendingCoinDetails };
