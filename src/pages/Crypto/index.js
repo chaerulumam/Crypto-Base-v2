97bf992030f0d7fbcc67f93ace85dc70a032b74b
Chaerul Umam <chaerulumamb244@gmail.com>
2023-02-28 23:22:37 +0700

    modify

--- src/pages/Crypto/index.js
+++ src/pages/Crypto/index.js
@@ -4,7 +4,7 @@ import { FiltersItem, TableItem } from "../../components";
 
 const Crypto = () => {
   return (
-    <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
+    <section className="xs:w-[80%] w-[90%] h-full flex flex-col mt-16 mb-24 relative">
       <FiltersItem />
       <TableItem />
       <Outlet />
